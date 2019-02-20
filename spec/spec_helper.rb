eae21a7439b1f361bd520c4754b382a557dd5549
Bruno Parga <brunoparga@gmail.com>
2019-02-20 20:45:21 -0300

    Install Capybara
    It wasn't included by default due to generating the Rails app with the 
    --skip-test option on (for RSpec)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,7 +23,7 @@ RSpec.configure do |config|
   config.before(:each, type: :system, js: true) do
     driven_by :selenium_chrome_headless
   end
-  
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.

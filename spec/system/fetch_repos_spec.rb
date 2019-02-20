50ea6bf2bdf881e3c4aa0b3b4454102af33e76b4
Bruno Parga <brunoparga@gmail.com>
2019-02-20 20:46:03 -0300

    Write the first test
    DB schema created since the test wouldn't even run without it

---
+++ spec/system/fetch_repos_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+feature "Fetch starred repositories", type: :system do
+  scenario "given a Github username" do
+    visit "/"
+    fill_in "username", with: "brunoparga"
+    click_button "get repositories"
+    expect(page).to have_content "challenge-development"
+  end
+end

318b5a64d52a88bf51c2d432da5fc5a350b216a5
Bruno Parga <brunoparga@gmail.com>
2019-02-21 16:03:06 -0300

    Test the Tag model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,4 +3,6 @@
 class Tag < ApplicationRecord
   has_many :repository_tags
   has_many :repositories, through: :repository_tags
+
+  validates :name, presence: true, uniqueness: true
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -3,5 +3,15 @@
 require "rails_helper"
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:empty_tag) { FactoryBot.create(:tag, name: nil) }
+
+  it "has a name" do
+    expect { empty_tag }.to raise_error("Validation failed: Name can't be blank")
+  end
+
+  it "name is unique" do
+    FactoryBot.create(:tag, name: "rspec")
+
+    expect { FactoryBot.create(:tag) }.to raise_error("Validation failed: Name has already been taken")
+  end
 end

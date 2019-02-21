698f0ba4586ee49359a257e1f962719605f26299
Bruno Parga <brunoparga@gmail.com>
2019-02-20 23:13:49 -0300

    Test Repos view

--- spec/views/pages/repos.html.erb_spec.rb
+++ spec/views/pages/repos.html.erb_spec.rb
@@ -3,5 +3,35 @@
 require "rails_helper"
 
 RSpec.describe "pages/repos.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:repos) do
+    Array.new(5).map do
+      {
+        id: 6,
+        name: "bananas",
+        description: "fruity JavaScript framework",
+        html_url: "https://github.com/bananas/bananas",
+        language: "javascript"
+      }
+    end
+  end
+
+  context "when assigned a list of starred repos" do
+    it "shows the repo information" do
+      assign(:repos, repos)
+
+      render
+
+      expect(rendered).to have_link("bananas", href: "https://github.com/bananas/bananas")
+    end
+  end
+
+  context "when assigned an empty list of repos" do
+    it "shows a warning about the empty list" do
+      assign(:repos, [])
+
+      render
+
+      expect(rendered).to have_content("Oops, no starred repositories were found.")
+    end
+  end
 end

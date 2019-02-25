9b7275ac25faf1c68faac5b7945231257c5f2e8f
Bruno Parga <brunoparga@gmail.com>
2019-02-25 13:26:03 -0300

    Fail at setting the desired param

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+  provider :github,
+           ENV['GITHUB_KEY'],
+           ENV['GITHUB_SECRET'],
+           client_options: { authorize_url: "https://localhost:3000/login/oauth/authorize?allow_signup=false" }
 end

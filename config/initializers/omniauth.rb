c8883ca618cde26ed9ec5d91d606769304049dec
Bruno Parga <brunoparga@gmail.com>
2019-02-25 14:40:16 -0300

    Fix OAuth signup issue

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github,
-           ENV['GITHUB_KEY'],
-           ENV['GITHUB_SECRET'],
-           client_options: { authorize_url: "https://localhost:3000/login/oauth/authorize?allow_signup=false" }
+    ENV["GITHUB_KEY"],
+    ENV["GITHUB_SECRET"], {
+      client_options: {
+        authorize_url: "https://github.com/login/oauth/authorize?allow_signup=false"
+      }
+    }
 end

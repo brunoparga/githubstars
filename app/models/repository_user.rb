32fa347de5917d36a78429d5d3b2655088cfc277
Bruno Parga <brunoparga@gmail.com>
2019-02-26 15:54:44 -0300

    Run Rubocop

--- app/models/repository_user.rb
+++ app/models/repository_user.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RepositoryUser < ApplicationRecord
   belongs_to :repository
   belongs_to :user
--- spec/factories/repository_users.rb
+++ spec/factories/repository_users.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :repository_user do
     repository { nil }

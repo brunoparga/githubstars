51be175089188819689fec27beaee08b52745507
Bruno Parga <brunoparga@gmail.com>
2019-02-26 17:47:43 -0300

    Improve the table view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def message(repos, search_tag)
+    if @repos.empty? && @search_tag
+      :empty_search
+    elsif @repos.empty?
+      :no_starred_repos
+    else
+      :found_repos
+    end
+  end
+
   def tags(repo, user)
     repo.repository_tags.select { |repo_tag| repo_tag.user == user }
   end

75f5146ca02203031b14b3302fc74abb6f512d3b
Bruno Parga <brunoparga@gmail.com>
2019-02-26 17:47:11 -0300

    Fix bug with session username

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,16 +1,25 @@
 # frozen_string_literal: true
 
 class RepositoriesController < ApplicationController
+  before_action :set_user
+
   def index
-    session[:username] = helpers.sanitize(params[:username]).strip
     @repos = ReposFetcher.fetch(session[:username])
   end
 
   def search
     @search_tag = params.permit(:tag_name)[:tag_name]
-    @searched_user = searched_user
     @repos = Search.browser(@search_tag, @searched_user)
     @repos = @searched_user.repositories if @search_tag.empty?
     render "repositories/index"
   end
+
+  private
+
+    def set_user
+      session[:username] = helpers
+                            .sanitize(params[:username])
+                            .strip if params[:username]
+      @searched_user = searched_user
+    end
 end

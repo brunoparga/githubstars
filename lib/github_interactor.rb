17de9be21f95ddff4ac9c58eeb2e782d4f912ef7
Bruno Parga <brunoparga@gmail.com>
2019-02-27 16:59:04 -0300

    Remove "crutch" of preloaded repos

--- lib/github_interactor.rb
+++ lib/github_interactor.rb
@@ -2,12 +2,8 @@
 
 module GithubInteractor
   def self.fetch_starred(username)
-    if %w[brunoparga ssaunier gustavohenrique].include? username
-      starred_repos = File.read("#{username}")
-    else
-      url = "https://api.github.com/users/#{username}/starred?sort=updated&direction=desc"
-      starred_repos = RestClient.get(url)
-    end
+    url = "https://api.github.com/users/#{username}/starred?sort=updated&direction=desc"
+    starred_repos = RestClient.get(url)
     JSON.parse(starred_repos, symbolize_names: true)
       .map { |repo| repo.slice(*%i[id name description html_url language]) }
       .each do |repo|
